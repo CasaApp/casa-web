f749829e2ea424ec76ba901bcba5003a2d163403
Karel Vuong <karelvuong@me.com>
2014-09-20 00:37:50 -0400

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+      jshint: {
+        gruntfile: {
+          src: 'Gruntfile.js'
+        }
+      },
+      compass: {
+        dist: {
+          options: {
+            cssDir: 'css',
+            outputStyle: 'compressed',
+            sassDir: 'sass',
+            specify: 'sass/app.scss'
+          }
+        }
+      },
+      watch: {
+        options: {
+          interrupt: true,
+        },
+        gruntfile: {
+          files: '<%= jshint.gruntfile.src %>',
+          tasks: ['jshint:gruntfile']
+        },
+        css: {
+          files: ['sass/*.scss', 'sass/*/*.scss'],
+          tasks: ['compass']
+        }
+      }
+    }
+  );
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['compass', 'watch']);
+};
---
+++ js/directives.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('dimensions.directives', [])
+.directive('holder', function ($compile) {
+    return {
+        link: function (scope, element, attrs) {
+            element.addClass('placeholder');
+            Holder.run({ images: element[0], nocss: true });
+        }
+    };
+})
+.directive("ngFileSelect",function(){
+    return {
+        link: function($scope, el){
+            el.bind("change", function(e){
+                $scope.file = (e.srcElement || e.target).files[0];
+                $scope.getFile();
+            })
+        }
+    }
+})
