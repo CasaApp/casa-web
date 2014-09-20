c0ce33d54d8b0bdb2686670b0be1f3432c954707
Karel Vuong <karelvuong@me.com>
2014-09-20 05:43:47 -0400

    xD

--- js/controllers.js
+++ js/controllers.js
@@ -13,22 +13,27 @@ angular.module('casa.controllers', [])
 .controller('ListingsController', function($scope) {
     $scope.listings = [
         {
+            "cover": "http://cdn.designhomes.pics/design/www.furnituregraphic.com/wp-content/uploads/2011/10/New-York-City-Apartment-1.jpg",
             "address": "49 Columbia St. W",
             "city": "Waterloo, ON"
         },
         {
+            "cover": "http://crumnyc.com/wp-content/uploads/2012/09/New-York-City-Apartments-for-Rent.jpg",
             "address": "49 Columbia St. W",
             "city": "Waterloo, ON"
         },
         {
+            "cover": "http://www.architecturaldigest.com/decor/2012-02/steven-harris-lucien-rees-roberts-new-york-apartment-article/_jcr_content/par/cn_contentwell/par-main/cn_pagination_container/cn_image.size.steven-harris-new-york-apartment-h670.jpg",
             "address": "49 Columbia St. W",
             "city": "Waterloo, ON"
         },
         {
+            "cover": "http://www.idesignarch.com/wp-content/uploads/Loft-Style-Apartment-Design-NY_1.jpg",
             "address": "49 Columbia St. W",
             "city": "Waterloo, ON"
         },
         {
+            "cover": "",
             "address": "49 Columbia St. W",
             "city": "Waterloo, ON"
         }
